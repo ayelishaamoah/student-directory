93a55f96510b5b58747d06138defc5966a823260
ayelisha amoah <ayelisha.amoah@gmail.com>
2019-03-27 09:15:37 +0000

    Added interactive menu

--- directory.rb
+++ directory.rb
@@ -1,4 +1,29 @@
 # put all of the students in an array
+def interactive_menu
+  students = []
+  # Loop indefinitely - until the program terminates
+  loop do
+  # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+  # 3. do what the user has asked
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)  
+      when "9"
+        exit
+      else
+        puts "I don't know what you mean, try again"
+    end
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish just hit return twice"
@@ -40,13 +65,13 @@ end
 
 def print(students)
   cohorts = students.map { |student| student[:cohort] }.uniq
-  cohorts.each do |cohort|
-    puts "#{cohort} Cohort:"
-    students.each do |student|
-      if student[:cohort] == cohort
-        puts "Name: #{student[:name]}, Age:#{student[:age]}, Country: #{student[:country]}"
+    cohorts.each do |cohort|
+      puts "#{cohort} Cohort:"
+      students.each do |student|
+        if student[:cohort] == cohort
+          puts "Name: #{student[:name]}, Age:#{student[:age]}, Country: #{student[:country]}"
+        end
       end
-    end
   end
 end
 
@@ -81,16 +106,4 @@ def print_footer(names)
   end
 end
 
-# We need to call the methods with any required arguements
-# Methods don't have access to local variables that are defined outside of them
-
-students = input_students
-print_header
-print(students)
-print_footer(students)
-=begin
-# Exercise 8.2 print by letter
-print_by_letter(students)
-# Exercise 8.3 print names less than 12 characters
-print_names_less_than_12(students)
-=end
+interactive_menu
