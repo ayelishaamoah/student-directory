8be955c7ebe84a181b9c75a2eb2b5d45db4966d0
ayelisha amoah <ayelisha.amoah@gmail.com>
2019-03-27 16:44:19 +0000

    Added method for loading data from a file

--- directory.rb
+++ directory.rb
@@ -12,6 +12,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -23,6 +24,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -49,6 +52,17 @@ def save_students
     file.puts csv_line
   end
   # Don't forget that we need to close the file!
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  # Read all of the lines in a file and return an array
+  file.readlines.each do |line|
+    # Each line with have a trailing newline character
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
   file.close
 end
 
